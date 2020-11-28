93c719903864e70a1707108715edd70b2dfd4afd
Sudipta-18 <64453329+Sudipta-18@users.noreply.github.com>
2020-11-28 17:54:01 +0530

    logical error check

--- tryy.php
+++ tryy.php
@@ -9,7 +9,7 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$nameErr = $emailErr = $genderErr = $websiteErr = 0;
 $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
